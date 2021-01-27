3565f62fde7a8d8b54754cb1184fcc109ebeddc6
David Crawford <dcrawford@spotinc.com>
2021-01-27 14:22:01 -0500

    Added grid and pan responder

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from "react-native";
+import LoginScreen from "./components/login";
 
 const App = () => {
-  return <View></View>;
+  return (
+    <SafeAreaView>
+      <LoginScreen />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
