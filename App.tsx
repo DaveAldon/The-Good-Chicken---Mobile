ffc3cfa0563f7c4b429d5e2bb0fc8f4c6ebdb8c4
David Crawford <dcrawford@spotinc.com>
2021-01-27 12:41:52 -0500

    Initial react-native init with my custom prettier settings

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from "react-native";
+
+const App = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
