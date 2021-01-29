5da75fbd0b9719c68465b2ea52cb7ecb569f6aac
David Crawford <dcrawford@spotinc.com>
2021-01-29 16:10:33 -0500

    Spells are now sortable by dragging

--- src/components/game/controller/inventory.tsx
+++ src/components/game/controller/inventory.tsx
@@ -1,19 +1,59 @@
-import React, { useRef, useState, useEffect } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, LogBox } from "react-native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import Feather from "react-native-vector-icons/Feather";
-import DraggableFlatList from "react-native-draggable-flatlist";
+import DraggableFlatList, { RenderItemParams } from "react-native-draggable-flatlist";
+
+LogBox.ignoreLogs(["ReactNativeFiberHostComponent:", "FlatList: Calling getNode()"]);
 
 const buttonSize = 75;
 const iconSize = buttonSize - 15;
 
+interface ISpell {
+  name: string;
+  color: string;
+  icon: any;
+  unlocked: boolean;
+}
+
+const spells: ISpell[] = [
+  {
+    name: "Chicken Flame",
+    color: "#ff4747",
+    icon: <FontAwesome5 name="fire-alt" size={iconSize} color="black" />,
+    unlocked: true,
+  },
+  {
+    name: "Egg",
+    color: "#ffff8f",
+    icon: <FontAwesome5 name="egg" size={iconSize} color="black" />,
+    unlocked: true,
+  },
+  {
+    name: "Toilet",
+    color: "#c94b0c",
+    icon: <FontAwesome5 name="toilet" size={iconSize} color="black" />,
+    unlocked: true,
+  },
+];
+
 export default function Inventory(props: any) {
+  const [data, setData] = useState(spells);
+
+  const renderSpell = useCallback(({ item, index, drag, isActive }: RenderItemParams<ISpell>) => {
+    return (
+      item.unlocked && (
+        <TouchableOpacity key={index} style={[{ backgroundColor: item.color }, styles.buttonStyle]} onLongPress={drag}>
+          {item.icon}
+        </TouchableOpacity>
+      )
+    );
+  }, []);
+
   return (
     <View style={{ backgroundColor: "#bfbfbf", height: "100%", borderWidth: 2, flexDirection: "row", borderColor: "grey" }}>
-      <View style={{ flexDirection: "column", width: "50%", justifyContent: "space-between" }}>
-        <FireButton {...props} />
-        <EggButton {...props} />
-        <ToiletButton {...props} />
+      <View style={{ flexDirection: "column", width: "50%", justifyContent: "space-between", padding: 5 }}>
+        <DraggableFlatList scrollEnabled={false} data={data} renderItem={renderSpell} keyExtractor={(item, index) => `draggable-item-${item.name}`} onDragEnd={({ data }) => setData(data)} />
       </View>
       <View style={{ flexDirection: "column", width: "50%" }}>
         <SettingsButton {...props} />
@@ -22,28 +62,9 @@ export default function Inventory(props: any) {
   );
 }
 
-function FireButton(props) {
-  return (
-    <TouchableOpacity style={[{ backgroundColor: "#ff4747" }, styles.buttonStyle]}>
-      <FontAwesome5 name="fire-alt" size={iconSize} color="black" />
-    </TouchableOpacity>
-  );
-}
-
-function EggButton(props) {
-  return (
-    <TouchableOpacity style={[{ backgroundColor: "#ffff8f" }, styles.buttonStyle]}>
-      <FontAwesome5 name="egg" size={iconSize} color="black" />
-    </TouchableOpacity>
-  );
-}
-
-function ToiletButton(props) {
-  return (
-    <TouchableOpacity style={[{ backgroundColor: "#c94b0c" }, styles.buttonStyle]}>
-      <FontAwesome5 name="toilet" size={iconSize} color="black" />
-    </TouchableOpacity>
-  );
+function Spell(props: ISpell) {
+  const { color, icon } = props;
+  return <TouchableOpacity style={[{ backgroundColor: color }, styles.buttonStyle]}>{icon}</TouchableOpacity>;
 }
 
 function SettingsButton(props) {
@@ -62,5 +83,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 10,
     margin: 0,
+    marginBottom: 5,
   },
 });
